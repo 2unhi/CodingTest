096bf472caa5168b1236d899918cd6723a2a4209
2unhi <ironhak1106@gmail.com>
2024-08-22 23:51:41 +0900

    2024.08.22

--- Python/BAEKJOON/Bronze I/2775.py
+++ Python/BAEKJOON/Bronze I/2775.py
@@ -1,12 +1,19 @@
+# DP 테이블 준비
+dp = [[0] * 15 for _ in range(15)]
+
+# 0층 거주민 수 초기화
+for i in range(1, 15):
+    dp[0][i] = i
+
+# DP 테이블 채우기
+for k in range(1, 15):
+    for n in range(1, 15):
+        dp[k][n] = dp[k][n-1] + dp[k-1][n]
+
+# 테스트 케이스 입력
 T = int(input())
 
 for _ in range(T):
     k = int(input())
     n = int(input())
-
-    people = [i for i in range(1, n+1)]
-
-    for _ in range(k):
-        for i in range(1, n):
-            people[i] += people[i-1]
-    print(people[-1])
+    print(dp[k][n])
---
+++ Python/BAEKJOON/Bronze IV/31403.py
@@ -0,0 +1,7 @@
+A = int(input())
+B = int(input())
+C = int(input())
+
+print(A+B-C)
+sum_AB = str(A) + str(B)
+print(int(sum_AB)-C)
